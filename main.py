8cf29afca47fda05ec08fe8e28ba5dc5f57a6dea
raon <91876178+E22EEE@users.noreply.github.com>
2024-01-11 15:42:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from config import Config
 from pyrogram import Client
 from pyrogram import  filters,enums
 from pyrogram.enums import ChatMemberStatus
@@ -18,4 +18,4 @@ plugins = dict(root="plugins")
 Client("raad",
 api_id=1263966,
 api_hash="6ae148a39b2074da28fa7e98c7f7e094",
-bot_token="5939440211:AAF4l2owVIORseALk8E_t23oFqPmOqIfpfU", plugins=plugins).run()
+bot_token=Config.TG_BOT_TOKEN, plugins=plugins).run()
