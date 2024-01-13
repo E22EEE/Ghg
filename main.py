6b8c4f5cdef3c8a57b3b57f8e56f4c68f5ead630
raon <91876178+E22EEE@users.noreply.github.com>
2024-01-13 19:26:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,63 @@
 from config import Config
-from pyrogram import Client, filters, idle, types
-import re
+import websocket
+import ssl
+import os
+import json
+import gzip
+import requests
+from time import sleep
+import random
+import concurrent.futures
+
+created=0
+failed=0
 
 
-app = Client(
-    "name",
-    13251350,
-    "66c0eacb36f9979ae6d153f207565cd6",
-    bot_token=Config.TG_BOT_TOKEN,
-    in_memory=True
-)
+G = '\033[1;32m'
+R = '\033[1;31m'
+own_id='2139948716'
+tele_bot=Config.TG_BOT.TOKEN
+ch='qwertyuioplkjhgfdsazxcvbnm1234567890'
+def create():
+	global created
+	global failed
+	user=str(random.choice('qwertyuioplkjhgfdsazxcvbnm')[0])+str(''.join(random.choice(ch) for i in range(12)))
+	
+	#user='kdvdfejevfsheh'
+	
+	headers = {
+	    "app": "com.safeum.android",
+	    "host": None,
+	    "remoteIp": "134.209.93.148",
+	    "remotePort": str(8080),
+	    "sessionId": "b6cbb22d-06ca-41ff-8fda-c0ddeb148195",
+	    "time": "2023-04-30 12:13:32",
+	    "url": "wss://51.79.208.190/Auth"
+	}
+	
+	
+	data0={"action":"Register","subaction":"Desktop","locale":"en_GB","gmt":"+02","password":{"m1x":"503c73d12b354f86ff9706b2114704380876f59f1444133e62ca27b5ee8127cc","m1y":"6387ae32b7087257452ae27fc8a925ddd6ba31d955639838249c02b3de175dfc","m2":"219d1d9b049550f26a6c7b7914a44da1b5c931eff8692dbfe3127eeb1a922fcf","iv":"e38cb9e83aef6ceb60a7a71493317903","message":"0d99759f972c527722a18a74b3e0b3c6060fe1be3ad53581a7692ff67b7bb651a18cde40552972d6d0b1482e119abde6203f5ab4985940da19bb998bb73f523806ed67cc6c9dbd310fd59fedee420f32"},"magicword":{"m1x":"04eb364e4ef79f31f3e95df2a956e9c72ddc7b8ed4bf965f4cea42739dbe8a4a","m1y":"ef1608faa151cb7989b0ba7f57b39822d7b282511a77c4d7a33afe8165bdc1ab","m2":"4b4d1468bfaf01a82c574ea71c44052d3ecb7c2866a2ced102d0a1a55901c94b","iv":"b31d0165dde6b3d204263d6ea4b96789","message":"8c6ec7ce0b9108d882bb076be6e49fe2"},"magicwordhint":"0000","login":str(user),"devicename":"Xiaomi Redmi Note 8 Pro","softwareversion":"1.1.0.1380","nickname":"hvtctchnjvfxfx","os":"AND","deviceuid":"c72d110c1ae40d50","devicepushuid":"*dxT6B6Solm0:APA91bHqL8wxzlyKHckKxMDz66HmUqmxCPAVKBDrs8KcxCAjwdpxIPTCfRmeEw8Jks_q13vOSFsOVjCVhb-CkkKmTUsaiS7YOYHQS_pbH1g6P4N-jlnRzySQwGvqMP1gxRVksHiOXKKP","osversion":"and_11.0.0","id":"1734805704"}
+	
+	ws=websocket.create_connection("wss://51.79.208.190/Auth", header=headers, sslopt={"cert_reqs": ssl.CERT_NONE})
+	ws.send(json.dumps(data0))
+	result=ws.recv()
+	decoded_data = gzip.decompress(result)
+	#print(G+str(decoded_data))
+	if '"comment":"Exists"' in str(decoded_data):
+		failed+=1
+	elif '"status":"Success"' in str(decoded_data):
+		created+=1
+		y = requests.post(f"""https://api.telegram.org/bot{tele_bot}/sendmessage?chat_id={own_id}&text={user}""")
+	elif '"comment":"Retry"' in str(decoded_data):
+		failed+=1
+	else:
+		print(decoded_data)
 
-app.start()
-start_string = '''
-مرحبًا بك {}
 
-انا بوت نسخ محتوى من اي قنوات مقيجوة المحتوى 
+executor=concurrent.futures.ThreadPoolExecutor(max_workers=100)
 
-فقط ارسل رابط المنشور من قناة عامة 
-
-مثال: `https://t.me/telegram/10`
-'''
-@app.on_message(filters.text & filters.private)
-async def on_text(c: Client, m: types.Message):
-    text = m.text
-    if re.findall("((www\.|http://|https://)(www\.)*.*?(?=(www\.|http://|https://|$)))", text):
-        url = re.findall("((www\.|http://|https://)(www\.)*.*?(?=(www\.|http://|https://|$)))", text)[0][0]
-        print(url)
-        if "t.me/" in url:
-            if "c/" in url:
-                return await m.reply("ارسل ربط من قناة عامه", quote=True)
-            else:
-                channel = url.split("t.me/")[1].split("/")[0]
-                msg_id = int(url.split("t.me/")[1].split("/")[1])
-                reply = await m.reply("انتظر ....", quote=True)
-                msg = await c.get_messages(channel, msg_id)
-                await reply.delete()
-                if not msg.chat.has_protected_content:
-                    return await m.reply("المنشور غير مقيد", quote=True)
-                if msg.text:
-                    return await m.reply(msg.text.html, quote=True, reply_markup=msg.reply_markup)
-                if msg.media_group_id:
-                    return await c.copy_media_group(m.chat.id, msg.chat.id, msg.id)
-                if msg.media:
-                    return await msg.copy(m.chat.id, reply_markup=msg.reply_markup)
-        else:
-            return await m.reply("لازم رابط منشور من قناة", quote=True)
-    else:
-        return await m.reply(start_string.format(m.from_user.mention))
-
-print(app.me.username)
-idle()
+while True:
+	executor.submit(create)
+	os.system('clear')
+	print(G+'Created : '+str(created))
+	print(R+'Failed : '+str(failed))
